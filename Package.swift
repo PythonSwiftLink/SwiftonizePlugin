f9e12062905130a661240aa2afe752c9566c0703
psychowasp <psychowaspx@gmail.com>
2024-09-16 11:36:21 +0200

    updated to temporary use swiftonize_dev

--- Package.swift
+++ Package.swift
@@ -22,25 +22,14 @@ let package = Package(
 		
         //.executable(name: "swiftonize", targets: ["swiftonize"]),
 		//.executable(name: "SwiftonizeExec", targets: ["SwiftonizeExec"]),
-        .plugin(name: "Swiftonizer", targets: ["SwiftonizeBuilder"]),
-		//.plugin(name: "SwiftonizerExt", targets: ["SwiftonizeBuilderExt"]),
-		//.plugin(name: "SwiftonizerNew", targets: ["SwiftonizeBuilderNew"]),
-		//.executable(name: "RunPlaygrounds", targets: ["Playgrounds"])
+        .plugin(name: "Swiftonize", targets: ["SwiftonizeBuilder"]),
     ],
     dependencies: [
         //.package(url: "https://github.com/PythonSwiftLink/Swiftonize", branch: "testing"),
-        //.package(path: "../Swiftonize-development"),
+//        .package(path: "../Swiftonize"),
 //        .package(url: "https://github.com/kylef/PathKit", from: .init(1, 0, 0)),
 //        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
 //        .package(url: "https://github.com/tomlokhorst/XcodeEdit", from: "2.9.0"),
-		//.package(url: "https://github.com/PythonSwiftLink/PythonSwiftLink-development", branch: "master"),
-		//.package(path: "../PythonSwiftLink-development"),
-		//.package(path: "../Swiftonize"),
-		.package(url: "https://github.com/kylef/PathKit", from: .init(1, 0, 0) ),
-		//.package(url: "https://github.com/apple/swift-syntax", from: .init(509, 0, 0) ),
-		
-		
-		//.package(url: "https://github.com/apple/swift-argument-parser", from: .init(1, 2, 0))
     ],
     targets: [
 
@@ -49,49 +38,8 @@ let package = Package(
             capability: .buildTool(),
             dependencies: [
 				//"swiftonize",
-				//"SwiftonizeExecutable"
 			]
         ),
-		//.plugin(name: <#T##String#>, capability: .command(intent: <#T##PluginCommandIntent#>, permissions: <#T##[PluginPermission]#>))
-//		.executableTarget(
-//			name: "Playgrounds",
-//			dependencies: [
-//				.product(name: "PySwiftCore", package: "PythonSwiftLink-development"),
-//				//.product(name: "SwiftonizeNew", package: "Swiftonize"),
-//				//.product(name: "PyWrapper", package: "Swiftonize"),
-//				.product(name: "PySwiftObject", package: "PythonSwiftLink-development"),
-//				"PathKit",
-//				"PythonResources"
-//			],
-//			plugins: ["SwiftonizeBuilderNew"]
-//		),
-//		.target(
-//			name: "PythonResources",
-//			resources: [
-//				.copy("python-stdlib"),
-//				.copy("python-extra")
-//			]
-//		),
-//		.target(
-//			name: "SwiftonizeTest",
-//			dependencies: [
-//				"SwiftonizeFW"
-//			]
-//		),
-//			.executableTarget(
-//				name: "SwiftonizeExecutable",
-//				dependencies: [
-////					.product(name: "SwiftSyntax", package: "swift-syntax"),
-////					.product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
-//					.product(name: "ArgumentParser", package: "swift-argument-parser"),
-//					//.product(name: "PySwiftCore", package: "PythonSwiftLink-development"),
-////					.product(name: "SwiftonizeNew", package: "Swiftonize-development"),
-////					.product(name: "PyWrapper", package: "Swiftonize-development"),
-//					//.product(name: "PySwiftObject", package: "PythonSwiftLink"),
-//					"PathKit",
-//					//"PythonResources"
-//				]
-//			),
-		//.binaryTarget(name: "SwiftonizeFW", path: "SwiftonizeFW.xcframework")
+		
     ]
 )
